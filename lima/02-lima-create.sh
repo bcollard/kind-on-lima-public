47cb705213cf06d467244e69955b8273a0766dc2
Jona Apelbaum <jona.apelbaum@gmail.com>
2023-01-18 16:21:21 +0100

    Update 02-lima-create.sh

--- lima/02-lima-create.sh
+++ lima/02-lima-create.sh
@@ -8,7 +8,7 @@ macosversion=$(sw_vers -productVersion)
 
 if [ $arch == "arm64" ]; then
     if [ $LIMA_INSTANCE == "vz" ]; then
-        if  [ $macosversion < 13 ]; then
+        if  [[ $macosversion < 13 ]]; then
             echo "Rosetta is not supported on arm64 on macOS 12 or earlier"
             exit 1
         fi
@@ -23,7 +23,7 @@ fi
 ${LIMACTL_BIN} start ./lima/${LIMA_INSTANCE}.yaml --tty=false --name ${LIMA_INSTANCE}
 
 if [ $arch == "arm64" ] && [ $LIMA_INSTANCE == "docker" ]; then
-    lima sudo nerdctl run --privileged --rm tonistiigi/binfmt --install all
+    ${LIMA_BIN} sudo nerdctl run --privileged --rm tonistiigi/binfmt --install all
 fi
 
 ${LIMA_BIN} -- sudo sysctl fs.inotify.max_user_watches=524288
